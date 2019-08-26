37985a89f69caeaa29b0d36cc49bd847e09b8553
John Chen <johnchen456@gmail.com>
2019-08-26 10:08:39 -0400

    fix: removed unecessary tasks and files

--- molecule/default/tests/test_default.rb
+++ molecule/default/tests/test_default.rb
@@ -1,1 +1,13 @@
 # frozen_string_literal: true
+
+describe file('/etc/sudoers.d/dmesg') do
+  it { should exist }
+end
+
+describe file('/root/.bashrc') do
+  it { should exist }
+end
+
+describe file('/root/.bash_profile') do
+  it { should exist }
+end
